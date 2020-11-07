fe09f40315139e26d56d10e6406b69f1678f783d
Karly Maruyama <karlypaige@yahoo.com>
2020-11-07 13:26:16 -0800

    pushing books.js back up to booksAPI branch

--- books.js
+++ books.js
@@ -2,16 +2,16 @@
 
 
 function favoriteBook(userFavorite) {
-var title=userFavorite.trim().split(' ').join('+');
-var apiKey="AIzaSyBSs5kWswhDUiDMYdYxtnVKl8h-wbTTxRw";
-var queryURL="https://www.googleapis.com/books/v1/volumes?q="+ title + "&maxResults=" + 1 + "&key=" + apiKey;
+    var title=userFavorite.trim();
+    var apiKey="AIzaSyBSs5kWswhDUiDMYdYxtnVKl8h-wbTTxRw";
+    var queryURL="https://www.googleapis.com/books/v1/volumes?q="+ title + "&maxResults=" + 1 + "&key=" + apiKey;
 
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(books){
-    console.log(books);
-});
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(books){
+        console.log(books);
+    });
 
 };
