1fe218c0e8bd8d199c0f6e5dbcef22eeaed7fe1e
Karly Maruyama <karlypaige@yahoo.com>
2020-11-10 21:54:06 -0800

    mapping book genres

--- books.js
+++ books.js
@@ -1,7 +1,10 @@
 var apiKey="AIzaSyBSs5kWswhDUiDMYdYxtnVKl8h-wbTTxRw";
+var allGenreArray = ['action','adventure','comedy','crime','drama','family','fantasy','history','horror','mystery','romance','science-fiction','thriller','war','western'];
+var bookGenreArray = ['Action','Adventure','Comedy','Crime','Drama','Juvenile Fiction','Young Adult Fiction','Biography & Autobiography','Horror','Mystery','Romance','Science-Fiction','Thriller','War','Western'];
+    
 
+function favoriteBook(userFavorite, newSearch) {
 
-function favoriteBook(userFavorite) {
     var title=userFavorite.trim();  
     var queryURL="https://www.googleapis.com/books/v1/volumes?q="+ title + "&maxResults=" + 1 + "&key=" + apiKey;
 
@@ -10,7 +13,7 @@ function favoriteBook(userFavorite) {
         url: queryURL,
         method: "GET"
     }).then(function(favorites){
-        // console.log(favorites);
+    
         //diplay chosen favorite
         
         let favorite = favorites.items[0].volumeInfo
@@ -19,12 +22,14 @@ function favoriteBook(userFavorite) {
         $("#favorite-poster").attr("src", favorite.imageLinks.thumbnail);
         $("#favorite-rating").text(favorite.categories[0] + " (" + favorite.maturityRating + ")");
         $("#favorite-plot").text(favorite.description);
-        $("#favorite-score").text(favorite.averageRating);
+        $("#favorite-score").text("Average book rating: " + favorite.averageRating);
         $("#favorite-full-url").attr("href", favorite.canonicalVolumeLink);
 
+        console.log(favorites);
         // console.log(favorite.canonicalVolumeLink);
-        console.log(pickGenreFromBook(favorites));
-        searchBooksByGenre("Drama");
+        if(newSearch){
+            pickGenreFromBook(favorites);
+        };
     });
 };
 
@@ -32,14 +37,36 @@ function favoriteBook(userFavorite) {
 //https://www.googleapis.com/books/v1/volumes?q=+subject:Young_Adult
 
 function pickGenreFromBook(obj){
-    return obj.items[0].volumeInfo.categories[0];
+    var bookGenre = obj.items[0].volumeInfo.categories[0];
+    var passedGenre;
+    console.log("bookGenre is: " + bookGenre);
+    //convert book Genre to allGenreArray
+    for (var b=0; b<bookGenreArray.length; b++) {
+        if (bookGenreArray[b] === bookGenre) {
+            //capture the index
+            passedGenre = allGenreArray[b];
+            break;
+        } else {
+            passedGenre ='drama';
+        }
+    }
+
+    console.log("passedGenre is: " + passedGenre)
+    //Pass genre to other medias
+    //book
+    searchBooksByGenre(passedGenre);
+    //video games
+    videoGameFromOther(passedGenre);
+    //anime
+    genreConvertID(passedGenre);
+    //movie
+    movieFromOtherMedia(passedGenre);
+
 }
 
 function searchBooksByGenre(genre) {
-    console.log("you have entered searchBookByGenre.  Genre is: " + genre)
+    console.log("searchBookByGenre Genre is: " + genre)
     var apiKey="AIzaSyBSs5kWswhDUiDMYdYxtnVKl8h-wbTTxRw";
-    var allGenreArray = ['action','adventure','comedy','crime','drama','family','fantasy','history','horror','mystery','romance','science-fiction','thriller','war','western'];
-    var bookGenreArray =['Fantasy','Fantasy','Fantasy','Fantasy','Drama','Fantasy','Fantasy','Fantasy','Fantasy','Fantasy','Romance','Fantasy','Fantasy','Fantasy','Fantasy'];
     let bookGenre = '';
 
     //convert passed genre to equivalent book genre
@@ -67,7 +94,7 @@ function searchBooksByGenre(genre) {
         $("#book-poster").attr("src", book.imageLinks.thumbnail);
         $("#book-rating").text(book.categories[0] + " (" + book.maturityRating + ")");
         $("#book-plot").text(book.description);
-        $("#book-score").text(book.averageRating);
+        $("#book-score").text("Average book rating: " + book.averageRating);
         $("#book-full-url").attr("href", book.canonicalVolumeLink);
 
         // console.log(book.canonicalVolumeLink);
